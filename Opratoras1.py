81c64d92c0827522e14277392c9bb9e9cc1131e1
CodeingHaki <vehanchilla@gmail.com>
2026-03-19 18:59:31 +0000

    demo

---
+++ Opratoras1.py
@@ -0,0 +1,10 @@
+tree1 = 12
+tree2 = 36
+tree3 = 67
+tree4 = 38
+tree5 = 11
+sum=tree1+tree2+tree3+tree4+tree5
+print("sum of all the trees is:", sum)
+
+average=sum//5
+print("the avarage of all the trees is:", average)
