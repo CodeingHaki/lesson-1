2adaeefc68de36cb85768cbfab5f34587aca971c
CodeingHaki <vehanchilla@gmail.com>
2026-03-20 19:54:41 +0000

    demo

---
+++ hw.3.py
@@ -0,0 +1,5 @@
+tempreture= float(input("tell me the tempreture and i will recomed what to wear:"))
+if tempreture< 20:
+    print("best to wear a coat")
+else:
+    print("best to wear shorts and t shrits")
