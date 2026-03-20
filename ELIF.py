a6604cbc4e1b34d7d0e3d75404426580cfb8b118
CodeingHaki <vehanchilla@gmail.com>
2026-03-20 19:00:15 +0000

    demo

---
+++ ELIF.py
@@ -0,0 +1,8 @@
+i= int(input("enter an number : "))
+if (i < 15):
+    print(i," is smaller than 15")
+    print("i'm in a if block")
+else:
+    print (i," is greater than 15")
+    print("i am an else block")
+print("i am not in a if block and not in the else block")
