f598421494d0882b006664c2289191d390d70a5d
CodeingHaki <vehanchilla@gmail.com>
2026-03-20 19:40:26 +0000

    demo

---
+++ hw2.py
@@ -0,0 +1,9 @@
+brithday1=2013
+brithday2=2009
+brithday3=1971
+brithday4=1982
+
+print("i was born in the year",brithday1)
+print("my brother is born in the year",brithday2)
+print("my dad is born in the year",brithday3)
+print("my mom is born in the year",brithday4)
