c0af58e6f8cbe9217c264588a76220aa906da499
CodeingHaki <vehanchilla@gmail.com>
2026-03-26 19:01:35 +0000

    demo

---
+++ operators/More operators.py
@@ -0,0 +1,21 @@
+a= 10
+b= 12
+c= 20
+
+if a and b and c:
+    print("All the numbers have boolean value")
+else:
+    print("At least one number has boolen value")
+
+A=10
+B=23
+C= 3
+
+if A > 0 or C > 0:
+    print("Either of the number i geater than 0")
+else:
+    print("No number is gerater than 0  ")
+if B > 0 or C > 0:
+    print("Either of the number is greater than 0")
+else:
+    print("No number is grater than 0")
---
+++ operators/more oporators2.py
@@ -0,0 +1,27 @@
+a= 10
+b=12
+c=12
+
+print(a !=b)
+print(c !=b)
+
+A= "python"
+B="coding"
+
+if A != B:
+    print(A,"and",B,"are different.")
+
+
+
+ab=4
+bc=5
+
+if(a==1)!= (b==5):
+    print("hello")
+
+cb=int(input("entter a number"))
+
+if cb%2 !=0:
+    print(cb,"is not even number")
+
+
