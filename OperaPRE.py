f864b227b250ccb579b31befe355d7afec29aea4
CodeingHaki <vehanchilla@gmail.com>
2026-04-02 20:01:29 +0100

    demo

---
+++ OperaPRE.py
@@ -0,0 +1,14 @@
+v=4
+x=8
+y=2
+z=0
+w=5
+z=(v+w)*x/y;
+print("value of (v+w)* x/y is",z)
+
+name="Alex"
+age= 10
+if name == "Alex" or name == "John" and age >=2:
+    print("hello! welcome.")
+else:
+    print("goodbye")
