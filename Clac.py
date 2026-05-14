7e8550a7c8624b07566cc9d19fe5a0a857243c5f
CodeingHaki <vehanchilla@gmail.com>
2026-05-14 19:51:32 +0100

    demo

---
+++ Clac.py
@@ -0,0 +1,28 @@
+def add(P,Q):
+    return P+Q
+def subtract(P,Q):
+    return P-Q
+def multiply(P,Q):
+    return P*Q
+def devide(P,Q):
+     return P/Q
+
+print("PLease select the operation.")
+print("a.add")
+print("b.subtract")
+print("c.multiply")
+print("d.devide")
+choice=input("Please enter the choice (a /b /c / d)")
+num1=int(input("please enter the first number:"))
+num2=int(input("please enter the second number:"))
+if choice=="a":
+    print(num1,"+",num2,"=",add(num1,num2))
+elif choice=="b":
+    print(num1,"-",num2,"=",subtract(num1,num2))
+elif choice=="c":
+    print(num1,"*",num2,"=",multiply(num1,num2))
+elif choice=="d":
+    print(num1,"/",num2,"=",devide(num1,num2))
+else:
+    print("invaild input")
+
