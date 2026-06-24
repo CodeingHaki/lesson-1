84c156f5c691e2304025370ebc44cb6467b206d3
CodeingHaki <vehanchilla@gmail.com>
2026-06-24 10:48:31 +0100

    demo

---
+++ Tupels.py
@@ -0,0 +1,15 @@
+tuplex=("tuple",False,3.2,1)
+print(tuplex)
+tuplex2=(4,6,2,8,3,1)
+print(tuplex2)
+tuplex2=tuplex2+(9,)
+print(tuplex2)
+
+tuple1=(50,10,60,70,50)
+print(tuple1.count(50))
+
+tuplex3=(2,4,3,5,4,6,7,8,6,1)
+slice=tuplex3[3:5]
+print(slice)
+slice=tuplex[:6]
+print(slice)
