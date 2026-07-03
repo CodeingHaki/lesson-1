e43f6e5838523f7392e497d2f6f46d396de00f81
CodeingHaki <vehanchilla@gmail.com>
2026-07-03 20:06:38 +0100

    demo

---
+++ Sets.py
@@ -0,0 +1,14 @@
+num_set={1,2,3}
+print(num_set)
+
+my_set={1.0,"Hello",(1,2,3)}
+print(num_set)
+num_set={1,2,3,4,3,2}
+print(num_set)
+
+num_set = set([1,2,3,4,5])
+print("orginal set:")
+print(num_set)
+num_set.pop()
+print('after removing the first element from the said set:')
+print(num_set,"\n")
---
+++ set intersections.py
@@ -0,0 +1,8 @@
+setx={"green","blue"}
+sety={"blue","yellow"}
+print("orginal set elements:")
+print(setx)
+print(sety)
+print("\nIntersetion of two said sets:")
+setz=setx.intersection(sety)
+print(setz)
